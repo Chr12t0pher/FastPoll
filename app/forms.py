c32d7c87a93fa39772ec7b5689b06a66dd9e29f9
Chris Stevens <chris27stevens@gmail.com>
2015-02-22 11:15:50 +0000

    Options are now validated

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,25 @@
-from wtforms import StringField, RadioField, SelectField, TextAreaField
+from wtforms import StringField, BooleanField, TextAreaField
+from wtforms.validators import ValidationError
 from flask_wtf import Form
-from .models import Poll
-from app import db
+from flask import flash
 
 
 class CreatePoll(Form):
     title = StringField("Title")
     desc = StringField("Description")
     options = TextAreaField("Options")
+    public = BooleanField("Public")
+
+    def validate_options(self, field):
+        options = []
+        for option in field.data.split("\n"):
+            options.append(option.strip("\r"))
+        if len(options) != len(set(options)):
+            flash("You have duplicate options.", "danger")
+            raise ValidationError("Duplicate Options")
+        if len(options) <= 1:
+            flash("Please have at least 2 options.", "info")
+            raise ValidationError("Not Enough Options")
+        if len(options) > 10:
+            flash("Please have no more than 10 options.", "info")
+            raise ValidationError("Too Many Options")
