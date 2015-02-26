3b75687ebd7364e4047fafb0c452c318fc5f0bc5
Chris Stevens <chris27stevens@gmail.com>
2015-02-26 16:34:08 +0000

    Updated

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import unittest
 import uuid
-from json import dumps
+from json import dumps, loads
 from app import app, db
 from app.models import Poll
 
@@ -18,18 +18,28 @@ class FastPollTestCase(unittest.TestCase):
     def test_isOnline(self):
         assert "FastPoll" in str(self.app.get("").data)  # The site loads successfully.
 
-    def test_newPoll(self):
+    def test_Poll(self):
         new_poll = Poll(id=str(uuid.uuid4().int >> 64),
                         title="Title",
                         desc="Description",
                         options=dumps(["Option 1", "Option 2"]),
-                        votes=dumps([]),
+                        votes=dumps({"Option 1": 0, "Option 2": 0}),
                         voters=dumps([]),
                         public=False,
                         ip=True)
         db.session.add(new_poll)
         db.session.commit()
         assert Poll.query.filter_by(title="Title").first() is not None  # We can add Polls to a database.
+
+        test_poll = Poll.query.first()
+        test_poll.votes = loads(test_poll.votes)
+        test_poll.votes["Option 1"] += 1
+        test_poll.votes = dumps(test_poll.votes)
+        db.session.add(test_poll)
+        db.session.commit()
+        assert loads(Poll.query.first().votes)["Option 1"] == 1  # We can get a Poll and vote on it.
+
+
 
 
 if __name__ == "__main__":
